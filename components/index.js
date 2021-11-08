195c7cb6201b53bc37222c6f5c6145bbb5cbf2ef
Rahul <pradhanrahul3346@gmail.com>
2021-11-08 22:26:17 +0530

    small mistake

--- components/index.js
+++ components/index.js
@@ -8,3 +8,4 @@ export { default as Comments } from './Commets';
 export { default as Author } from './Author';
 export { default as PostDetail } from './PostDetail';
 export { default as FeaturedPostsCard } from './FeaturedPostsCard';
+export { default as Loader } from './Loader';
